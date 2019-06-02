e32b852ec72133396f891b9ddf7e6c957dc77732
Jeremy Campbell <jcampbell2@harding.edu>
2019-06-01 23:02:11 -0700

    Init. Webpack configuration needs tweaking.

---
+++ src/server/index.js
@@ -0,0 +1,8 @@
+// Entry point for Server
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.listen(8080, () => console.log('Listening on port 8080...'));
